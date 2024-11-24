3140b0bdec724b3a5c0fc9325c4df6da8a24e522
dong3 <147096452+sink0324@users.noreply.github.com>
2024-11-25 02:50:14 +0900

    Update 첨탑 밀어서 부수기.py

--- 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.py
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.py
@@ -4,7 +4,7 @@ cnt = 1
 
 for i in range(n-1):
     if len(top) == 1: break
-    if top[i] <= top[i+1]:
+    if top[i] <= top[i+1]: #<= 이어야 함. 같아도 넘어지지 않기 때문. 부등호를 잘 생각하자.
         cnt += 1
     else:
         continue
