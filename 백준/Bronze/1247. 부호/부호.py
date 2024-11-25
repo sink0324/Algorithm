6e3eac4f395a589ee41b7a7f3c69fd3e909a019e
dong3 <147096452+sink0324@users.noreply.github.com>
2024-11-26 01:46:25 +0900

    [Bronze III] Title: 부호, Time: 180 ms, Memory: 41900 KB -BaekjoonHub

--- 백준/Bronze/1247. 부호/부호.py
+++ 백준/Bronze/1247. 부호/부호.py
@@ -1,9 +1,8 @@
+from sys import stdin
+
 for i in range(3):
     n = int(input())
-    s = 0
-    for j in range(n):
-        integer = int(input())
-        s += integer
-    if s == 0: print(0)
-    elif s < 0: print('-')
-    elif s > 0: print('+')
+    li = [int(stdin.readline()) for i in range(n)]
+    if sum(li) == 0: print(0)
+    elif sum(li) < 0: print('-')
+    elif sum(li) > 0: print('+')
