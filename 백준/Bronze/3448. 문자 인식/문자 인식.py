84ae9a6f1e1abcaf249277c54b5eb9b56e2a274d
dong3 <147096452+sink0324@users.noreply.github.com>
2025-01-19 22:49:47 +0900

    [Bronze I] Title: 문자 인식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/3448. 문자 인식/문자 인식.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+n = int(input())
+
+for i in range(n):
+    sentence = ''
+    for line in stdin:
+        if line == '\n':
+            sentence = sentence.rstrip('\n')
+            break
+        else:
+            sentence += line.rstrip('\n')
+    A = len(sentence)
+    R = A - sentence.count('#')
+    res = round(R/A * 100, 1)
+    if str(res).split('.')[-1] == '0':
+        res = int(res)
+
+    print(f"Efficiency ratio is {res}%.")
+
+#블로그 참고함
